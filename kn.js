7dcb16fd1804b89f3f83f4b5db4c30ba9eccd8cd
katelyj <kjohnston@stuy.edu>
2017-03-09 14:51:28 -0500

    sort of work

--- kn.js
+++ kn.js
@@ -8,21 +8,36 @@ var makeDot = function(x, y) {
 
     dot.setAttribute("cx", x);
     dot.setAttribute("cy", y);
-    dot.setAttribute("r", 10);
+    dot.setAttribute("r", 30);
     dot.setAttribute("fill", "green");
+
+    dot.addEventListener("click", clickDot);
 
     return dot;
 
 }
 
-var addDot = function(e) {
-    svg.appendChild( makeDot( e.offsetX, e.offsetY ) );
+var addDot = function(x, y) {
+    svg.appendChild( makeDot( x, y ) );
 }
 
 var moveDot = function(e) {
     circles = document.getElementsByTagName("circle");
 }
 
-svg.addEventListener("click", addDot);
+var clickDot = function(e) {
+    if ( this.getAttribute("fill") != "pink" ) {	
+	this.setAttribute("fill","pink");
+	e.stopPropagation();
+    }
+    
+    else {
+	this.remove();
+	var newCircle = addDot(Math.random() * svg.clientWidth, Math.random() * svg.clientHeight);
+    };
+    
+};
+
+svg.addEventListener("click", function(e) { addDot(e.offsetX, e.offsetY) });
 svg.addEventListener("go", moveDot);
 document.getElementById("clear").addEventListener("click", clearFrame);
