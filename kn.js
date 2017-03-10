2f63e6d52ffa056656e27cbbe49f387143c160a6
goyyapazham <nalanda99@gmail.com>
2017-03-10 06:33:06 -0500

    done

--- kn.js
+++ kn.js
@@ -1,3 +1,5 @@
+var rid = 0;
+
 var clearFrame = function() {
     svg.innerHTML = '';
 }
@@ -11,33 +13,73 @@ var makeDot = function(x, y) {
     dot.setAttribute("r", 30);
     dot.setAttribute("fill", "green");
 
+    dot.xv = 1;
+    dot.yv = 1;
+
     dot.addEventListener("click", clickDot);
 
     return dot;
 
 }
 
-var addDot = function(x, y) {
-    svg.appendChild( makeDot( x, y ) );
+var addDot = function(e) {
+    svg.appendChild( makeDot( e.offsetX, e.offsetY ) );
 }
 
 var moveDot = function(e) {
-    circles = document.getElementsByTagName("circle");
+
+    window.cancelAnimationFrame(rid);
+
+    var woo = function() {
+
+	var circles = document.getElementsByTagName("circle");
+	
+	for( var i = 0; i < circles.length; i++ ) {
+
+	    var x = parseInt( circles[i].getAttribute("cx") );
+	    var y = parseInt( circles[i].getAttribute("cy") );
+
+	    if( x < 30 || x > svg.clientWidth - 30 )
+		circles[i].xv *= -1;
+
+	    if( y < 30 || y > svg.clientHeight - 30 )
+		circles[i].yv *= -1;
+
+	    circles[i].setAttribute( "cx", x + circles[i].xv );
+	    circles[i].setAttribute( "cy", y + circles[i].yv );
+
+	}
+
+	rid = window.requestAnimationFrame(woo);
+
+    }
+
+    woo();
+
+    //setInterval( woo, 15 );
+	
 }
 
 var clickDot = function(e) {
-    if ( this.getAttribute("fill") != "pink" ) {	
-	this.setAttribute("fill","pink");
-	e.stopPropagation();
+    
+    if ( this.getAttribute("fill") != "pink" ) {
+	this.setAttribute("fill", "pink");
     }
     
     else {
-	this.remove();
-	var newCircle = addDot(Math.random() * svg.clientWidth, Math.random() * svg.clientHeight);
+	
+	svg.removeChild( this );
+	
+	var newDot = makeDot( Math.random() * svg.clientWidth,
+			      Math.random() * svg.clientHeight );
+	
+	svg.appendChild( newDot );
     };
+
+    e.stopPropagation();
     
 };
 
-svg.addEventListener("click", function(e) { addDot(e.offsetX, e.offsetY) });
-svg.addEventListener("go", moveDot);
+svg.addEventListener("click", addDot);
+go.addEventListener("click", moveDot);
 document.getElementById("clear").addEventListener("click", clearFrame);
